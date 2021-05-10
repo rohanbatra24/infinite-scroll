a32f98257b2cbab4b58d46e05e08055daa790966
Rohan Batra <rohanbatra24@gmail.com>
2021-05-10 18:02:19 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const imageContainer = document.getElementById("image-container");
+
+const imageContainer2 = document.getElementById("image-container");
+
+const loader = document.getElementById("loader");
+
+let photosArray = [];
+
+// create image elements and add to DOM
+const displayImages = () => {
+  photos.forEach((photo) => {
+    const image = document.createElement("img");
+    image.src = photo.urls.raw;
+    imageContainer.appendChild(image);
+  });
+};
+
+// Get new images from Unsplash
+const getNewImages = async () => {
+  const response = await fetch(
+    "https://api.unsplash.com/photos/random/?client_id=XjeEk0WpC6b4l0Y91Z4fR4Sw7QN2aiRAraitJVOZqOA&count=10"
+  );
+
+  photos = await response.json();
+
+  displayImages();
+};
+
+getNewImages();
